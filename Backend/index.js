8127f0da132f8ed9fef79afe7983d78959bdffe9
Siddharth Jain <PAREEN.RATHORE00@GMAIL.COM>
2024-02-05 00:27:55 +0530

    origin update

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://sportify-pari.netlify.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
